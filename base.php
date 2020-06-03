3bf748a79c363b819d7388494ade725afa7c509b
張 建文 <chang180@gmail.com>
2020-06-03 23:28:50 +0800

    practice-5

--- base.php
+++ base.php
@@ -1,20 +1,18 @@
 <!-- 練習時腦袋要清醒一點 -->
 <!-- 最好是能專心再練，但更好的是能邊打瞌睡也做得出來 -->
-
 <?php
-
 session_start();
 date_default_timezone_set("Asia/Taipei");
 
 class DB{
-private $dsn="mysql:host=localhost;charset=utf8;dbname=invoice";
-private $pdo;
 private $table;
+private $dsn="mysql:host=localhost;dbname=invoice;charset=utf8";
+private $pdo;
 private $root="root";
 private $password="";
 public function __construct($table){
-    $this->table=$table;
     $this->pdo=new PDO($this->dsn,$this->root,$this->password);
+    $this->table=$table;
 }
 
 // search data
@@ -30,41 +28,41 @@ public function all(...$arg){
     return $this->pdo->query($sql)->fetchAll();
 }
 
-// delete data
+//delete data
 public function del($arg){
     $sql="DELETE FROM $this->table ";
     if(is_array($arg)){
-        foreach($arg as $key => $value){
-                $tmp[]=sprintf("`%s`='%s'",$key,$value);
-        }
-    $sql.=" WHERE ".implode(" && ",$tmp);
-    }else $sql.=" WHERE `id`='$arg'";
-    return $this->pdo->exec($sql);
-}
-
-// search specific data
-public function find($arg){
-    $sql="SELECT * FROM $this->table ";
-    if(is_array($arg)){
-        foreach($arg as $key => $value){
+        foreach ($arg as $key => $value){
             $tmp[]=sprintf("`%s`='%s'",$key,$value);
         }
         $sql.=" WHERE ".implode(" && ",$tmp);
     }else $sql.=" WHERE `id`='$arg'";
+    return $this->pdo->exec($sql);
+}
+
+//search specific data
+public function find($arg){
+    $sql="SELECT * FROM $this->table ";
+    if(is_array($arg)){
+        foreach($arg as $key=>$value){
+            $tmp[]=sprintf("`%s`='%s'",$key,$value);
+        }
+        $sql.=" WHERE ".implode(" && ",$tmp);
+    }else $sql.=" WHERE id='$arg'";
     return $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
 }
 
 // count data numbers
 public function count(...$arg){
     $sql="SELECT COUNT(*) FROM $this->table ";
-    if(!empty($arg[0])){
-        foreach($arg[0] as $key=>$value){
+    if(!empty($arg[0]) && is_array($arg[0])){
+        foreach ($arg as $key => $value){
             $tmp[]=sprintf("`%s`='%s'",$key,$value);
         }
         $sql.=" WHERE ".implode(" && ",$tmp);
     }
     if(isset($arg[1])) $sql.=$arg[1];
-return $this->pdo->query($sql)->fetchColumn();
+    return $this->pdo->query($sql)->fetchColumn();
 }
 
 //query
@@ -75,26 +73,24 @@ public function q($sql){
 //insert or update data
 public function save($arg){
     if(isset($arg['id'])){
-        foreach($arg as $key=>$value){
-            if($key!='id'){
-                $tmp[]=sprintf("`%s`='%s',$key,$value");
-            }
+        foreach ($arg as $key=>$value){
+            $tmp[]=sprintf("`%s`='%s'",$key,$value);
         }
+        //update
         $sql="UPDATE $this->table SET ".implode(",",$tmp)." WHERE `id`='".$arg['id']."'";
-    }else $sql="INSERT INTO $this->table  (`".implode("`,`",array_keys($arg))."`) VALUES ('".implode("','",$arg)."')";
+        //insert
+    }else $sql="INSERT INTO $this->table (`".implode("`,`",array_keys($arg))."`) VALUES ('".implode("','",$arg)."')";
     return $this->pdo->exec($sql);
 }
 
-
-
-
-
 }
 
-//direct
+// direct
 function to($url){
-    header("location:".$url);
+header("location:".$url);
 }
+
+
 
 $inv=new DB("invoice");
 $an=new DB('award_number');
