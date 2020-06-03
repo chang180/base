be6366eab9d79d3c830ff201e61acfd1a18b7dd7
張 建文 <chang180@gmail.com>
2020-06-03 18:23:13 +0800

    practice-2

--- base.php
+++ base.php
@@ -3,105 +3,94 @@
 date_default_timezone_set("Asia/Taipei");
 session_start();
 
-class DB
-{
-
-    private $dsn = "mysql:host=localhost;dbname=invoice;charset=utf8";
+class DB{
     private $pdo;
     private $table;
-    public function __construct($table)
-    {
-        $this->table = $table;
-        $this->pdo = new PDO($this->dsn, 'root', '');
-    }
-
-    // search data
-    function all(...$arg)
-    {
-        $sql = "SELECT * FROM $this->table ";
-        if (isset($arg[0]) && is_array($arg[0])) {
-            foreach ($arg[0] as $key => $value) {
-                $tmp[] = sprintf("`%s`='%s'", $key, $value);
-            }
-            $sql .= " WHERE " . implode(" && ", $tmp);
-        }
-        if (isset($arg[1])) $sql .= $arg[1];
-        return $this->pdo->query($sql)->fetchAll();
-    }
-
-    //delete data
-    function del($arg)
-    {
-        $sql = "DELETE FROM $this->table ";
-        if (is_array($arg)) {
-            foreach ($arg as $key => $value) {
-                $tmp[] = sprintf("`%s`='%s`", $key, $value);
-            }
-            $sql .= " WHERE " . implode(" && ", $tmp);
-        } else $sql .= " WHERE `id`='$arg'";
-        return $this->pdo->exec($sql);
-    }
-
-
-
-
-    // search specific data
-    function find($arg)
-    {
-        $sql = "SELECT * FROM $this->table ";
-        if (is_array($arg)) {
-            foreach ($arg as $key => $value) {
-                $tmp[] = sprintf("`%s`='%s'", $key, $value);
-            }
-            $sql .= " WHERE " . implode(" && ", $tmp);
-        } else $sql .= " WHERE `id`='$arg'";
-        return $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
-    }
-
-    //count data numbers
-    function nums(...$arg)
-    {
-        $sql = "SELECT COUNT(*) FROM $this->table ";
-        if (isset($arg[0]) && is_array($arg[0])) {
-            foreach ($arg[0] as $key => $value) {
-                $tmp[] = sprintf("`%s`='%s'", $key, $value);
-            }
-            $sql .= " WHERE " . implode(" && ", $tmp);
-        }
-        if (isset($arg[1])) $sql .= $arg[1];
-
-        return $this->pdo->query($sql)->fetchColumn();
-    }
-
-    //query
-    function q($sql)
-    {
-        return $this->pdo->query($sql)->fetchAll();
-    }
-
-    //insert or update data
-    function save($arg)
-    {
-        if (isset($arg['id'])) {
-            foreach ($arg as $key => $value) {
-                if ($key != $arg['id']) {
-                    $tmp[] = sprintf("`%s`='%s'", $key, $value);
-                }
-            }
-            $sql = "UPDATE " . $this->table . " SET " . implode(',', $tmp) . " WHERE `id`='" . $arg['id'] . "'";
-        } else $sql = "INSERT INTO " . $this->table . " (`" . implode("`,`", array_keys($arg)) . "`) VALUES ('" . implode("','", $arg) . "')";
-        return $this->pdo->exec($sql);
-    }
-
-//quick direct
-function to($url){
-    header("location:".$url);
+    private $dsn="mysql:host=localhost;dbname=invoice;charset=utf8";
+public function __construct($table){
+    $this->table=$table;
+    $this->pdo=new PDO($this->dsn,'root','');
 }
 
 
+// search data
+function all(...$arg){
+    $sql="SELECT * FROM $this->table ";
+    if(isset($arg[0]) && is_array($arg[0])){
+        foreach($arg[0] as $key => $value){
 
+            $tmp[]=sprintf("`%s`='%s'",$key,$value);
+        }
+        $sql.=" WHERE ".implode(" && ",$tmp);
+    }
+    if(isset($arg[1])) $sql.=$arg[1];
+    return $this->pdo->query($sql)->fetchAll();
+}
 
+// delete data
+function del($arg){
+$sql = "DELETE FROM $this->table ";
+if(is_array($arg)){
+    foreach($arg as $key => $value){
+        $tmp[]=sprintf("`%S`='%s'",$key,$value);
+    }
+$sql.=" WHERE ".implode(" && ",$tmp);
+}else $sql.=" WHERE id='$arg'";
+return $this->pdo->exec($sql);
+}
 
+// search specific data
+function find($arg){
+    $sql="SELECT * FROM $this->table ";
+    if(is_array($arg)){
+        foreach ($arg as $key => $value){
+
+            $tmp[]=sprintf("`%s`='%s'",$key,$value);
+        }
+    $sql.=" WHERE ".implode(" && ",$tmp);
+    }else $sql.=" WHERE `id`='$arg'";
+    return $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
+}
+
+// count data numbers
+function nums(...$arg){
+    $sql="SELECT COUNT(*) FROM $this->table ";
+    if(isset($arg[0]) && is_array($arg[0])){
+        foreach($arg[0] as $key => $value){
+            $tmp[]=sprintf("`%s`='%s'",$key,$value);
+        }
+        $sql.=" WHERE ".implode(" && ",$tmp);
+    }
+    if(isset($arg[1])) $sql.=$arg[1];
+    return $this->pdo->query($sql)->fetchColumn();
+}
+
+// query 
+function q($sql){
+    return $this->pdo->query($sql)->fetchAll();
+}
+
+// insert or update data
+function save($arg){
+    if(isset($arg['id'])){
+        foreach($arg as $key => $value){
+            if($key != $arg['id']){
+                $tmp[]=sprintf("`%s`='%s'",$key,$value);
+            }
+        }
+            $sql="UPDATE ".$this->table." SET ".implode(',',$tmp)." WHERE `id`='".$arg['id']."'";
+            
+    }else{
+        $sql="INSERT INTO ".$this->table." (`".implode("`,`",array_keys($arg))."`) VALUES ('".implode("','",$arg)."')";
+    }
+
+return $this->pdo->exec($sql);
+}
+
+// direct
+function to($url){
+    header("loaction:".$url);
+}
 }
 
 $inv=new DB("invoice");
@@ -116,5 +105,7 @@ print_r($row);
 
 echo "<hr>";
 print_r($ar);
+
+
 
 ?>
