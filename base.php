d0ce41b240e1be21127902a959f38afbcdfc7731
張 建文 <chang180@gmail.com>
2020-06-07 06:51:49 +0800

    finished

--- base.php
+++ base.php
@@ -2,36 +2,49 @@
 <!-- 最好是能專心再練，但更好的是能邊打瞌睡也做得出來 -->
 <?php
 session_start();
-date_default_timezone_set("Asia/Taipei");
+date_default_timezone_set(("Asia/Taipei"));
 
 class DB
 {
-    private $pdo;
-    private $dsn = "mysql:host=localhost;charset=utf8;dbname=invoice";
     private $table;
+    private $dsn = "mysql:host=localhost;charset=utf8;dbname=invoice";
+    private $pdo;
     private $root = "root";
     private $password = "";
     public function __construct($table)
     {
         $this->table = $table;
-        $this->pdo = new PDO($this->dsn,$this->root,$this->password); //PDO裏面不用再放""
+        $this->pdo = new PdO($this->dsn, $this->root, $this->password);
     }
-    //search data
+    // search data
     public function all(...$arg)
     {
         $sql = "SELECT * FROM $this->table ";
-        if (!empty($arg[0]) && is_array($arg[0])) {
+        if (isset($arg[0]) && is_array($arg[0])) {
             foreach ($arg[0] as $key => $value) {
-                $tmp[] = sprintf("`%s`='%s`", $key, $value);
+                $tmp[] = sprintf("`%s='%s", $key, $value);
             }
             $sql .= " WHERE " . implode(" && ", $tmp);
         }
-        if (!empty($arg[1])) $sql .= $arg[1];
+        if (isset($arg[1])) $sql .= $arg[1];
         return $this->pdo->query($sql)->fetchAll();
     }
 
-    //delete data
+    // delete data
     public function del($arg)
+    {
+        $sql = "DELETE FROM $this->table ";
+        if (is_array($arg)) {
+            foreach ($arg as $key => $value) {
+                $tmp[] = sprintf("`%s`='%s", $key, $value);
+            }
+            $sql .= " WHERE " . implode(" && ", $tmp);
+        } else $sql .= " WHERE id='$arg'";
+        return $this->pdo->exec($sql);
+    }
+
+    // search specific data
+    public function find($arg)
     {
         $sql = "SELECT * FROM $this->table ";
         if (is_array($arg)) {
@@ -40,27 +53,14 @@ class DB
             }
             $sql .= " WHERE " . implode(" && ", $tmp);
         } else $sql .= " WHERE id='$arg'";
-        return $this->pdo->exec($sql);
-    }
-
-    //search specific data
-    public function find($arg)
-    {
-        $sql = "SELECT * FROM $this->table ";
-        if (is_array($arg)) {
-            foreach ($arg as $key => $value) {
-                $tmp[] = sprintf("`%s`='%s',$key,$value");
-            }
-            $sql .= " WHERE " . implode(" && ", $tmp);
-        } else $sql .= "WHERE id='$arg'";
         return $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
     }
 
-    //count data numbers
+    // count data numbers
     public function count(...$arg)
     {
-        $sql = " SELECT COUNT(*) FROM $this->table ";
-        if (!empty($arg[0]) && is_array($arg[0])) {
+        $sql = "SELECT COUNT(*) FROM $this->table ";
+        if (isset($arg[0])) {
             foreach ($arg[0] as $key => $value) {
                 $tmp[] = sprintf("`%s`='%s'", $key, $value);
             }
@@ -70,7 +70,7 @@ class DB
         return $this->pdo->query($sql)->fetchColumn();
     }
 
-    // query
+    //query
     public function q($sql)
     {
         return $this->pdo->query($sql)->fetchAll();
@@ -79,25 +79,27 @@ class DB
     //insert or update data
     public function save($arg)
     {
-        if (isset($arg['id'])) { //特別注意是用['id']做判斷
+        if (isset($arg['id]'])) {
             foreach ($arg as $key => $value) {
-
-                if ($key != 'id') {
-                    $tmp[] = sprintf("`%s`='%s'", $key, $value);
-                }
+                $tmp[] = sprintf("`%s`='%s'", $key, $value);
             }
-            //UPDATE
-            $sql = "UPDATE $this->table SET " . implode(",", $tmp) . "WHERE id='" . $arg['id'] . "'";
-        } else $sql = "INSERT INTO $this->table (`" . implode("`,`", array_keys($arg)) . "`) VALUES ('" . implode("','", $arg) . "')";
+            //update
+            $sql = "UPDATE $this->table SET " . implode(",", $tmp) . " WHERE id='" . $arg['id'] . "'";
+            //insert
+        } else $sql = "INSERT INTO $this->table SET (`" . implode("`,`", array_keys($arg)) . "`) VALUES ('" . implode("','", $arg) . "')";
         return $this->pdo->exec($sql);
     }
 }
-
-//direct
+// direct
 function to($url)
 {
     header("location:" . $url);
 }
+
+
+
+
+
 
 
 
